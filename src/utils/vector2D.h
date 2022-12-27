24789ff5bd75129edb4a2bb2cb5db2d902183991
Thibault <92511341+TiboHrqn@users.noreply.github.com>
2022-12-27 18:11:38 +0100

    correction

--- src/utils/vector2D.h
+++ src/utils/vector2D.h
@@ -5,6 +5,8 @@
 class Vector2D {
     public:
         float x, y;
+        
+        static const Vector2D i2D, j2D;
 
     public:
         Vector2D(float x, float y) : x(x), y(y) {};
@@ -31,8 +33,6 @@ typedef Vector2D Point2D;
 class ReferenceFrame2D{
     public:
         Vector2D i, j;
-
-        static const Vector3D i2D, j2D;
     
     public:
         ReferenceFrame2D (Vector2D i, Vector2D j) : i(i.normalize()), j(j.normalize()) {};
