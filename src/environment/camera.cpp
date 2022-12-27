c90e2de0f311c37048403dd06baf52b09a473e1c
Thibault <92511341+TiboHrqn@users.noreply.github.com>
2022-12-27 18:12:48 +0100

    corrections

--- src/environment/camera.cpp
+++ src/environment/camera.cpp
@@ -6,17 +6,17 @@ Point3D Camera::getScreenCenter() {
 }
 
 Point3D Camera::getTopLeftCorner() {
-    return getScreenCenter() + (ScreenHeight / 2) * reference.j - (screenWidth / 2) * reference.k ;
+    return getScreenCenter() + (screenHeight / 2) * reference.j - (screenWidth / 2) * reference.k ;
 }
 
 Point3D Camera::getTopRightCorner() {
-    return getScreenCenter() + (ScreenHeight / 2) * reference.j + (screenWidth / 2) * reference.k ;
+    return getScreenCenter() + (screenHeight / 2) * reference.j + (screenWidth / 2) * reference.k ;
 }
 
 Point3D Camera::getBottomLeftCorner() {
-    return getScreenCenter() - (ScreenHeight / 2) * reference.j - (screenWidth / 2) * reference.k ;
+    return getScreenCenter() - (screenHeight / 2) * reference.j - (screenWidth / 2) * reference.k ;
 }
 
 Point3D Camera::getBottomRightCorner() {
-    return getScreenCenter() - (ScreenHeight / 2) * reference.j + (screenWidth / 2) * reference.k ;
+    return getScreenCenter() - (screenHeight / 2) * reference.j + (screenWidth / 2) * reference.k ;
 }
