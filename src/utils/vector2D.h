304ff70d127033c89436d39cd5dcc3ba497d630d
TiboHrqn <harquin.thibault@gmail.com>
2022-12-27 11:30:01 +0100

    .

--- src/utils/vector2D.h
+++ src/utils/vector2D.h
@@ -9,7 +9,7 @@ class Vector2D : public {
     public:
         Vector2D(float x, float y) : x(x), y(y) {};
         Vector2D() : Vector2D(0, 0) {};
-        Vector2D(Vector2D & p1, Vector2D & p2) : Vector3D(p2.x - p1.x, p2.y - p1.y) {};
+        Vector2D(Vector2D & p1, Vector2D & p2) : Vector2D(p2.x - p1.x, p2.y - p1.y) {};
 
         Vector2D operator+(const Vector2D &);
         Vector2D operator-(const Vector2D &);
