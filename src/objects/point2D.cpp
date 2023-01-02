015c30c3bd197951cef80e1a0d1f2c87773067ee
TiboHrqn <harquin.thibault@gmail.com>
2023-01-02 22:31:54 +0100

    j'ai commenté des trucs pour compiler

--- src/objects/point2D.cpp
+++ src/objects/point2D.cpp
@@ -1,11 +1,11 @@
-#include "Point2D.h"
+#include "point2D.h"
 #include <cmath>
 
 Point2D::Point2D(Camera camera, Point3D point3d){
-    const float angleX = camera.reference.i.angleHorizontal(point3d-camera.position);
-    const float angleY = camera.reference.i.angleVertical(point3d-camera.position);
+    const float angleX = camera.reference.i.angleHorizontal(point3d - camera.position);
+    const float angleY = camera.reference.i.angleVertical(point3d - camera.position);
 
     x = (int) cos(angleX) * camera.screenDistance + camera.screenWidth/2 +0.5;
     y = (int) cos(angleY) * camera.screenDistance + camera.screenHeight/2 +0.5;
-    z = (point3d-camera.position).norm();
+    z = (point3d - camera.position).norm();
 }
--- src/objects/point2D.h
+++ src/objects/point2D.h
@@ -15,8 +15,8 @@ class Point2D{
     public:
         Point2D(int x, int y, float z) : x(x), y(y) , z(z) {};
         Point2D(Point2D &p) : x(p.x), y(p.y), z(p.z) {};
-        Point2D(Camera camera, Point3D point3d);
-        
+        Point2D(Camera camera, Point3D point3d);   
+        Point2D() : x(0), y(0), z(0){};    
 
 };
 
